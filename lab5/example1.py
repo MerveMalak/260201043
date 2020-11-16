171b90b6097d484c5ae127542da81a760ef14ed7
t59493659 <mervemalak@std.iyte.edu.tr>
2020-11-16 08:59:17 +0000

    example1

--- lab5/example1.py
+++ lab5/example1.py
@@ -1,3 +1,12 @@
 number = int(input("Enter a integer: "))
-for i in range(1,11):
-    print(f"{number} * {i} = {number * i}")
+while True:
+  if number < 0:
+    print("Please enter valid number: ")
+    number = int(input("Enter a integer: "))
+  elif number > 10:
+    print("Please enter number <10: ")
+    number = int(input("Enter a integer: "))
+  else:
+    for i in range(1,11):
+      print(f"{number} * {i} = {number * i}")
+    break
