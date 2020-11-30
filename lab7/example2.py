d6228a6301f4e79b2d824b4881072a4701a03f8e
t59493659 <mervemalak@std.iyte.edu.tr>
2020-11-30 08:59:35 +0000

    example2.py

--- lab7/example2.py
+++ lab7/example2.py
@@ -2,5 +2,5 @@ books = ["ULYSSES", "ANIMAL FARM", "BRAVE NEW WORLD", "ENDER'S GAME"]
 book_dict = {}
 for book in books:
   book_dict[book] = (len(book),len(set(book)))
- for key,value in books:
-  print(key+" -> "+value)
+for current_key,current_value in book_dict.items():
+  print(current_key," -> ",current_value)
