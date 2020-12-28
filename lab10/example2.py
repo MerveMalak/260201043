051b24569a72d651dff5bb4a4e767bae9d0a27a2
t59493659 <mervemalak@std.iyte.edu.tr>
2020-12-28 09:11:18 +0000

    example2

--- lab10/example2.py
+++ lab10/example2.py
@@ -1,10 +1,10 @@
-def halistone(n):
+def hailstone(n):
   if n == 1:
     return "1"
   
   if n % 2 == 0:
-    return str(int(n)) + "," + str(halistone(n/2))
+    return str(int(n)) + "," + str(hailstone(n/2))
   
   else:
-    return str(int(n)) + "," + str(halistone(3*n + 1))
-print(halistone(11))
+    return str(int(n)) + "," + str(hailstone(3*n + 1))
+print(hailstone(11))
